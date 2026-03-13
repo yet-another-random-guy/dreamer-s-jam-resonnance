259518e400f0acb09033f4fa70cb017e0adec13d
some_random_guy <raphael.della-negra@etudiant.univ-rennes.fr>
2026-03-13 13:25:53 +0100

    Fini le script de mouvement (les constantes peuvent etre a revoir)

---
+++ bat.gd
@@ -0,0 +1,38 @@
+extends Node2D
+const acceleration = 10.0
+const air_res = 0.7
+const max_spd = 5.0
+
+var spd: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spd = Vector2.ZERO
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var accel = Vector2(0, 0)
+	
+	if Input.is_action_pressed("Right"):
+		accel.x += 1
+	if Input.is_action_pressed("Left"):
+		accel.x -= 1
+	if Input.is_action_pressed("Up"):
+		accel.y -= 1
+	if Input.is_action_pressed("Down"):
+		accel.y += 1
+		
+	accel = accel.normalized() * acceleration * delta
+	
+	if accel == Vector2.ZERO:
+		spd *= air_res
+	else:
+		spd += accel
+		if spd.length() > max_spd:
+			spd = spd.normalized() * max_spd
+	
+	position += spd
+	
+	pass
