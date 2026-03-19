8dcfa70567cb7cf210ca5cc1af91f8a0db870c80
some_random_guy <raphael.della-negra@etudiant.univ-rennes.fr>
2026-03-19 07:56:59 +0100

    Ajoute la bat pour tester les mouvements (Les collisions ne marchent pas)

--- Scripts/test_light.gd
+++ Scripts/test_light.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	$Light.position = get_viewport().get_mouse_position()
+	$Light.position = $Bat.position
 	pass
